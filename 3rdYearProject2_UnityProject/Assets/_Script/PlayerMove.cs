b0ebc1313f764badeb41bb7b6f4dbc194f8055ac
EvoneeHsieh <evonee.hsieh@hotmail.com>
2024-04-08 21:16:13 +0800

    刪註解

--- 3rdYearProject2_UnityProject/Assets/_Script/PlayerMove.cs
+++ 3rdYearProject2_UnityProject/Assets/_Script/PlayerMove.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float maxRotate = 5f; // 最大旋?角度
-    public float maxSpeed = 3f; // 最大移?速度
-    public float lerpAmt = 0.1f; // 插值系?
+    public float maxRotate = 5f; 
+    public float maxSpeed = 3f; 
+    public float lerpAmt = 0.1f; 
 
-    private Vector3 rVec; // 相机的右向量（用于旋?）
-    private Vector3 fVec; // 相机的前向量（用于移?）
+    private Vector3 rVec; 
+    private Vector3 fVec; 
 
     void Start()
     {
