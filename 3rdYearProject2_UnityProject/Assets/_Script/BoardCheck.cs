24bcb67e539d5bebca35aee7e5d53ecbf8a77797
Ediny0116 <87802953+Ediny0116@users.noreply.github.com>
2024-04-22 14:32:16 +0800

    分數計算完成
    下項目:連線亮燈

--- 3rdYearProject2_UnityProject/Assets/_Script/BoardCheck.cs
+++ 3rdYearProject2_UnityProject/Assets/_Script/BoardCheck.cs
@@ -7,9 +7,11 @@ public class BoardCheck : MonoBehaviour
 {
     public List<GameObject> ChessList=new List<GameObject>();
     public int[] LineCount=new int[12];
+    public GameObject ScoreUI;
 
     public void AddToLine(GameObject chess)
     {
+        ScoreUI.GetComponent<Score>().UpdateScore(1);
         int ChessNum=ChessList.IndexOf(chess);
         int posY = (ChessNum / 5)+5;
         int posX = ChessNum % 5;
@@ -28,19 +30,19 @@ public class BoardCheck : MonoBehaviour
 
         if (CheckIsLineFull(LineCount[posY]))
         {
-            
+            ScoreUI.GetComponent<Score>().UpdateScore(5);
         }
         if (CheckIsLineFull(LineCount[posX]))
         {
-            
+            ScoreUI.GetComponent<Score>().UpdateScore(5);
         }
         if (CheckIsLineFull(LineCount[10]))
         {
-            
+            ScoreUI.GetComponent<Score>().UpdateScore(5);
         }
         if (CheckIsLineFull(LineCount[11]))
         {
-            
+            ScoreUI.GetComponent<Score>().UpdateScore(5);
         }
     }
 
--- 3rdYearProject2_UnityProject/Assets/_Script/Score.cs
+++ 3rdYearProject2_UnityProject/Assets/_Script/Score.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
+    TextMeshProUGUI scoreUI;
+    string scoreString= "Score:";
+    int ScoreCount = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreUI = GetComponent<TextMeshProUGUI>();
+        scoreUI.text = scoreString + "0";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScore(int score)
     {
-        
+        ScoreCount += score;
+        scoreUI.text = scoreString + ScoreCount;
     }
 }
